68c9dbae8e37bec5ec4c1e34f5153c8c696ccd96
eitandanino <98532339+eitandanino@users.noreply.github.com>
2023-05-16 11:49:36 +0300

    added mongodb and a change from numbers to words

--- main.py
+++ main.py
@@ -7,6 +7,30 @@ from datetime import datetime
 apiToken = os.environ["API_TOKEN"]
 apiURL = f'https://api.telegram.org/bot{apiToken}/sendMessage'
 
+user_name = os.environ["USER_NAME"]
+mongo_db_password = os.environ["MONGO_DB_PASSWORD"]
+
+# Connect to MongoDB
+cluster = MongoClient(f"mongodb+srv://{user_name}:{mongo_db_password}@safirat-haomer.ynxy0yo.mongodb.net/")
+db = cluster["chat_ids"]
+collection = db["telegram_chat_ids"]
+
+def add_user_chat_id(chat_id):
+    # Check if the chat ID already exists in the database
+    result = collection.find_one({"_id": chat_id})
+    if result is None:
+        # Insert the chat ID if it doesn't exist
+        post = {"_id": chat_id}
+        collection.insert_one(post)
+
+
+def get_users_chat_ids_mongo():
+    # Retrieve all user chat IDs from the database
+    results = collection.find({})
+    chat_ids = [result["_id"] for result in results]
+    return chat_ids
+
+
 CURRENT_DAY = dates.HebrewDate.today().day
 CURRENT_MONTH = dates.HebrewDate.today().month
 
@@ -37,6 +61,17 @@ if (CURRENT_MONTH == 1 and CURRENT_DAY >= 14 or
     weeks = sefira_day // 7
 
 
+    def number_to_hebrew_words(number):
+        units = ["אפס", "אחד", "שני", "שלשה", "ארבעה", "חמשה", "ששה", "שבעה", "שמונה", "תשעה", "עשרה", "אחד עשר",
+                 "שנים עשר", "שלשה עשר", "ארבעה עשר", "חמשה עשר", "ששה עשר", "שבעה עשר", "שמונה עשר", "תשעה עשר",
+                 "עשרים", "אחד ועשרים", "שנים ועשרים", "שלשה ועשרים", "ארבעה ועשרים", "חמשה ועשרים",
+                 "ששה ועשרים", "שבעה ועשרים", "שמונה ועשרים", "תשעה ועשרים", "שלשים", "אחד ושלשים", "שנים ושלשים",
+                 "שלושה ושלשים", "ארבעה ושלשים", "חמשה ושלשים", "ששה ושלשים", "שבעה ושלשים", "שמונה ושלשים",
+                 "תשעה ושלשים", "ארבעים", "אחד וארבעים", "שנים וארבעים", "שלשה וארבעים", "ארבעה וארבעים",
+                 "חמשה וארבעים", "ששה וארבעים", "שבעה וארבעים", "שמונה וארבעים", "תשעה וארבעים"]
+        return units[number]
+
+
     def get_english_sentence(sefira_day, weeks):
         if weeks == 0:
             if sefira_day % 7 == 1:
@@ -63,12 +98,12 @@ if (CURRENT_MONTH == 1 and CURRENT_DAY >= 14 or
                        f" {sefira_day % 7} days of the Omer."
 
 
-    def get_hebrew_sentence(sefira_day, weeks):
+    def get_hebrew_sentence_safaradi(sefira_day, weeks):
         if weeks == 0:
             if sefira_day % 7 == 1:
                 return f"היום יום אחד לעומר"
             else:
-                return f"היום {sefira_day} ימים לעומר"
+                return f"היום {number_to_hebrew_words(sefira_day)} ימים לעומר"
         elif weeks == 1:
             if sefira_day % 7 == 0:
                 return f"היום שבעה ימים לעומר שהם שבוע אחד"
@@ -79,20 +114,49 @@ if (CURRENT_MONTH == 1 and CURRENT_DAY >= 14 or
             elif sefira_day % 7 == 3:
                 return f"היום עשרה ימים לעומר שהם שבוע אחד ושלשה ימים"
             else:
-                return f"היום {sefira_day} יום לעומר שהם {weeks} שבוע אחד ו" \
-                       f" {sefira_day % 7} ימים"
+                return f"היום {number_to_hebrew_words(sefira_day)} יום לעומר שהם {number_to_hebrew_words(weeks)} " \
+                       f"שבוע אחד ו{number_to_hebrew_words(sefira_day % 7)} ימים"
         else:
             if sefira_day % 7 == 0:
-                return f"היום {sefira_day} יום לעומר שהם {weeks} שבועות"
+                return f"היום {number_to_hebrew_words(sefira_day)} יום לעומר שהם {number_to_hebrew_words(weeks)} שבועות"
             elif sefira_day % 7 == 1:
-                return f"היום {sefira_day} יום לעומר שהם {weeks} שבועות ו" \
-                       f" {sefira_day % 7} יום"
+                return f"היום {number_to_hebrew_words(sefira_day)} יום לעומר שהם {number_to_hebrew_words(weeks)}" \
+                       f" שבועות ויום {number_to_hebrew_words(sefira_day % 7)}"
             else:
-                return f"היום {sefira_day} יום לעומר שהם {weeks} שבועות ו" \
-                       f" {sefira_day % 7} ימים"
-        
-             
-    def get_user_chat_ids():
+                return f"היום {number_to_hebrew_words(sefira_day)} יום לעומר שהם {number_to_hebrew_words(weeks)} " \
+                       f"שבועות ו{number_to_hebrew_words(sefira_day % 7)} ימים"
+
+
+    def get_hebrew_sentence_ashkenaz(sefira_day, weeks):
+        if weeks == 0:
+            if sefira_day % 7 == 1:
+                return f"היום יום אחד לעומר"
+            else:
+                return f"היום {number_to_hebrew_words(sefira_day)} ימים לעומר"
+        elif weeks == 1:
+            if sefira_day % 7 == 0:
+                return f"היום שבעה ימים שהם שבוע אחד לעומר"
+            elif sefira_day % 7 == 1:
+                return f"היום שמונה ימים שהם שבוע אחד ויום אחד לעומר"
+            elif sefira_day % 7 == 2:
+                return f"היום תשעה ימים שהם שבוע אחד ושני ימים לעומר"
+            elif sefira_day % 7 == 3:
+                return f"היום עשרה ימים שהם שבוע אחד ושלשה ימים לעומר"
+            else:
+                return f"היום {number_to_hebrew_words(sefira_day)} יום שהם {number_to_hebrew_words(weeks)} " \
+                       f"שבוע אחד ו{number_to_hebrew_words(sefira_day % 7)} ימים לעומר"
+        else:
+            if sefira_day % 7 == 0:
+                return f"היום {number_to_hebrew_words(sefira_day)} יום שהם {number_to_hebrew_words(weeks)} שבועות לעומר"
+            elif sefira_day % 7 == 1:
+                return f"היום {number_to_hebrew_words(sefira_day)} יום שהם {number_to_hebrew_words(weeks)}" \
+                       f" שבועות ויום {number_to_hebrew_words(sefira_day % 7)} לעומר"
+            else:
+                return f"היום {number_to_hebrew_words(sefira_day)} יום שהם {number_to_hebrew_words(weeks)} " \
+                       f"שבועות ו{number_to_hebrew_words(sefira_day % 7)} ימים לעומר"
+
+
+    def get_and_add_user_chat_ids():
         try:
             response = requests.get(f"https://api.telegram.org/bot{apiToken}/getUpdates")
             data = response.json()
@@ -100,24 +164,28 @@ if (CURRENT_MONTH == 1 and CURRENT_DAY >= 14 or
             for result in data["result"]:
                 if "message" in result:
                     chat_id = result["message"]["chat"]["id"]
-                    users_chat_ids.add(chat_id)
-            return list(users_chat_ids)
+                    add_user_chat_id(chat_id)
         except Exception as ee:
             print(f"Error getting user chat IDs: {ee}")
             return []
 
-        
-    user_chat_ids = get_user_chat_ids()
+
+    # Add new ids to the db
+    get_and_add_user_chat_ids()
+    # Get all the Chat ids from the db
+    all_users_chat_ids = get_users_chat_ids_mongo()
+
+    # Get all teh sentences
     all_sentence_english = get_english_sentence(sefira_day, weeks)
-    all_sentence_hebrew = get_hebrew_sentence(sefira_day, weeks)
+    all_sentence_hebrew_safaradi = get_hebrew_sentence_safaradi(sefira_day, weeks)
+    all_sentence_hebrew_ashkenaz = get_hebrew_sentence_ashkenaz(sefira_day, weeks)
 
-
-    #     print(get_english_sentence(sefira_day, weeks))
-    #     print(get_hebrew_sentence(sefira_day, weeks))
-    for user_chat_id in user_chat_ids:
+    # Send to each chat id
+    for user_chat_id in all_users_chat_ids:
         try:
             response = requests.post(apiURL, json={'chat_id': user_chat_id,
-                                                   'text': f'{all_sentence_hebrew} \n {all_sentence_english}'})
+                                                   'text': f'עדות המזרח - {all_sentence_hebrew_safaradi} \n אשכנז - '
+                                                           f'{all_sentence_hebrew_ashkenaz} \n '
+                                                           f'English - {all_sentence_english}'})
         except Exception as e:
             print(e)
-
